932722ae9c7c236a3af12ac60162758aa0a6b4b9
Rizqulloh Rayhan Ferdiansyah <rizqullohrayhan@student.uns.ac.id>
2023-12-21 11:19:32 +0700

    add table customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,30 @@
-import { Metadata } from "next";
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+import { CustomersTableSkeleton } from '@/app/ui/skeletons';
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
 
 export const metadata: Metadata = {
-    title: 'customers'
-}
+  title: 'customers',
+};
 
-export default function Page() {
-    return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+      <Suspense fallback={<CustomersTableSkeleton />}>
+        <CustomersTable customers={customers} />
+      </Suspense>
+  );
 }
--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -216,3 +216,63 @@ export function InvoicesTableSkeleton() {
     </div>
   );
 }
+
+export function CustomersTableSkeleton() {
+  return (
+    <div className="w-full">
+      <h1 className={`mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      {/* <Search placeholder="Search customers..." /> */}
+      <div className="mt-6 flow-root">
+        <div className="inline-block min-w-full align-middle">
+          <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
+            <div className="md:hidden">
+              <InvoicesMobileSkeleton />
+              <InvoicesMobileSkeleton />
+              <InvoicesMobileSkeleton />
+              <InvoicesMobileSkeleton />
+              <InvoicesMobileSkeleton />
+              <InvoicesMobileSkeleton />
+            </div>
+            <table className="hidden min-w-full text-gray-900 md:table">
+              <thead className="rounded-lg text-left text-sm font-normal">
+                <tr>
+                  <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+                    Customer
+                  </th>
+                  <th scope="col" className="px-3 py-5 font-medium">
+                    Email
+                  </th>
+                  <th scope="col" className="px-3 py-5 font-medium">
+                    Amount
+                  </th>
+                  <th scope="col" className="px-3 py-5 font-medium">
+                    Date
+                  </th>
+                  <th scope="col" className="px-3 py-5 font-medium">
+                    Status
+                  </th>
+                  <th
+                    scope="col"
+                    className="relative pb-4 pl-3 pr-6 pt-2 sm:pr-6"
+                  >
+                    <span className="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white">
+                <TableRowSkeleton />
+                <TableRowSkeleton />
+                <TableRowSkeleton />
+                <TableRowSkeleton />
+                <TableRowSkeleton />
+                <TableRowSkeleton />
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
